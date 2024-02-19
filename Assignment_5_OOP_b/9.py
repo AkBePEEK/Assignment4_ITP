28fd4ae14092270c7adc354bd6ffcc732ff8253b
AkBePEEK <231338@astanait.edu.kz>
2024-02-20 01:23:19 +0600

    first

--- Assignment_5_OOP_b/9.py
+++ Assignment_5_OOP_b/9.py
@@ -1,1 +1,12 @@
-class
+class OnesThreesNines:
+    def __init__(self, var):
+        self.answer = var
+
+    def __str__(self):
+        return f"nines:{self.answer // 9}, threes:{self.answer % 9 // 3}, ones:{self.answer % 9 % 3}"
+
+
+ones_threes_nines = OnesThreesNines
+print(ones_threes_nines(10))
+print(ones_threes_nines(15))
+print(ones_threes_nines(22))
